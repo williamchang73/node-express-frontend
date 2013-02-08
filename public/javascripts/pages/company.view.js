ac41c6b3fba6c1fb697e687b05925fcd36948e56
william chang <williamchang73@gmail.com>
2013-02-08 22:57:05 +0800

    apply dtat

--- public/javascripts/pages/company.view.js
+++ public/javascripts/pages/company.view.js
@@ -19,13 +19,46 @@ companyController.getCompanyData = function() {
         async : false,
         success : function(data) {
         	// success do nothing;
-			console.log(data); 
+			companyController.setCompanyInfo(data);		
+			companyController.setWorkingSpaces(data);	 
         },                 
         error : function(data) {
             console.error(data.error_message);
         }
 	});
 };
+
+//set up event
+companyController.setCompanyInfo = function(data) {
+	console.log(data);
+	var company_info = data.company_info;
+	$('.brand strong').text('Inside - ' + company_info.name);
+	$('#slogan h3 p').text(company_info.slogan);
+}
+
+
+//set up event
+companyController.setWorkingSpaces = function(data) {
+	var $workspace = $('#workspace').remove().clone();
+	var company_pic = data.compamy_pic;
+	var startIndex = 4;
+	$.each( company_pic, function(i, picinfo) {
+		if(++i>startIndex){
+	        var $eachPicinfo = $workspace.clone();
+	        console.log(picinfo.pic);
+	        $eachPicinfo.find("img").attr("src", picinfo.pic)
+	        .end()
+	        .find('.text h6')
+	        .text(picinfo.title)
+	        .end()
+	        .find('.text p')
+	        .text(picinfo.desc)
+	        .end();
+	        $('.features_op1_row').append( $eachPicinfo );
+		}
+    });
+}
+
 
 
 
