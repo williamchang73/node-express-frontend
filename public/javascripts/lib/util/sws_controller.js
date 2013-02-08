abeba3d5a36e5729c2908d8e123dc4de5fa94e11
william chang <williamchang73@gmail.com>
2013-02-08 21:04:51 +0800

    for using ejs include

---
+++ public/javascripts/lib/util/sws_controller.js
@@ -0,0 +1,16 @@
+function SWSController() {};
+SWSController.prototype.init = function() {};
+SWSController.prototype.initWidget = function() {};
+SWSController.prototype.initBinding = function() {};
+SWSController.prototype.exec = function() {};
+SWSController.prototype.initialize = function() {
+	try {
+		this.init();
+		this.initWidget();
+		this.initBinding();
+		this.exec();
+	}
+	catch(x) {
+		console.error(x.message);
+	}
+};
