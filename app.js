c7c43dd54e98b06a05c85db76880f0f522a05b24
william chang <williamchang73@gmail.com>
2013-03-30 10:46:19 +0800

    for all fies

--- app.js
+++ app.js
@@ -8,18 +8,25 @@ var express = require('express')
   , path = require('path')
   , Log = require('log')
   , log = new Log('info', fs.createWriteStream('./log/abous.log')
-  ,	engine = require('ejs-locals')  
-  
+  ,	engine = require('ejs-locals')
+  , piler = require("piler-compat")
 );
 
-  
 var app = express();
-log.info('app start');
+//var app = require('express');
+var srv = http.createServer(app);
+var clientjs = piler.createJSManager();
+//var clientcss = piler.createCSSManager();
+
+
 app.configure(function(){
+	
+  clientjs.bind(app, srv);
+  //clientcss.bind(app, srv);	
+
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('testing', 'testing');
-  //app.engine('htm', require('ejs').renderFile);
   app.engine('htm', engine);
   app.set('view engine', 'htm');
   app.use(express.favicon());
@@ -27,10 +34,18 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  //app.use(express.session());
   app.use(app.router);
   app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.set('clientjs', clientjs);
+  /* glocal js and css */
+  clientjs.addFile("app", __dirname + "/public/3rd_party/bootstrap/js/bootstrap.min.js");
+  clientjs.addFile("app", __dirname + "/public/3rd_party/cookies/jquery.cookie.js");
+  clientjs.addFile("app", __dirname + "/public/javascripts/default/theme.js");
+  clientjs.addFile("app", __dirname + "/public/javascripts/lib/util/swsController.js");
+  
 });
 
 app.configure('development', function(){
@@ -43,6 +58,9 @@ app.configure('development', function(){
 // Load configurations
 var env = process.env.NODE_ENV || 'development'
   , config = require('./config/config')[env]
+
+process.env.NODE_ENV = env;
+
 
 //for routing --------------------
 var routes = require('./routes')
@@ -66,6 +84,6 @@ app.get('/logout', login.logout);
 app.get('/admin/create_company', admin.createCompany);
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+srv.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port') +  " ... " + env);
 });
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
+  var js = req.app.get('clientjs');
+  
   var data = { 
-		
+		"global_js" : js.renderTags("app")
 	};
+	
   res.render('pages/index', data);
 };
